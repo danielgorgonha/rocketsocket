030b4cf69a49af07475af186adc18ebe037f2afe
Daniel R Gorgonha <daniel.gorgonha@dynamox.net>
2022-05-19 07:43:35 -0300

    feat: configuring database

--- src/http.ts
+++ src/http.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import path from "path";
 import { createServer } from "http";
-import { Server } from "socket.io"; 
+import { Server } from "socket.io";
+import mongoose from "mongoose";
 
 const app = express();
 
 const server = createServer(app);
+
+mongoose.connect("mongodb://localhost/rocketsocket");
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 
