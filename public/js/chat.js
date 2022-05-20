a7ed846a8bb8697eeb79d79463f753b75af6fa08
Daniel R Gorgonha <daniel.gorgonha@dynamox.net>
2022-05-19 22:36:09 -0300

    feat: creating user communication room

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,5 @@
 const socket = io("http://localhost:3000");
+let roomId = "";
 
 function onLoad() {
   const urlParams = new URLSearchParams(window.location.search);
@@ -54,5 +55,17 @@ function addUser(user) {
     </li>
   `;
 }
+
+document.getElementById("users_list").addEventListener("click", (e) => {
+  if (e.target && e.target.matches("li.user_name_list")) {
+    const idUser = e.target.getAttribute("idUser");
+    console.log("idUser", idUser);
+
+    socket.emit("start_chat", { idUser }, (data) => {
+      console.log(data);
+      roomId = data.room.idChatRoom;
+    });
+  }
+});
 
 onLoad();
