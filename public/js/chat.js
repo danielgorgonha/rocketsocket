0805a25eee6658f968996abce62dba2bd15c714b
Daniel R Gorgonha <daniel.gorgonha@dynamox.net>
2022-05-23 23:04:48 -0300

    feat: final adjustments

--- public/js/chat.js
+++ public/js/chat.js
@@ -62,7 +62,7 @@ function addMessage(data) {
         class="img_user"
         src=${data.user.avatar}
       />
-      <strong> ${data.user.name}</strong>
+      <strong> ${data.user.name} &nbsp;</strong>
       <span> ${dayjs(data.message.created_at).format("DD/MM/YYYY HH:mm")}</span>
     </span>
     <div class="messages">
@@ -89,9 +89,18 @@ function addUser(user) {
 }
 
 document.getElementById("users_list").addEventListener("click", (e) => {
+  const inputMessage = document.getElementById("user_message");
+  inputMessage.classList.remove("hidden");
+
+  document
+    .querySelectorAll("li.user_name_list")
+    .forEach((item) => item.classList.remove("user_in_focus"))
+
   document.getElementById("message_user").innerHTML = "";
   if (e.target && e.target.matches("li.user_name_list")) {
     const idUser = e.target.getAttribute("idUser");
+
+    e.target.classList.add("user_in_focus");
 
     const notification = document.querySelector(
       `#user_${idUser} .notification`
